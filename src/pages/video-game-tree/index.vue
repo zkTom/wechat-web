e53102f32a643a230c0a9aca7fcf56c072120a55
zkTom <1770102258@qq.com>
2021-08-13 15:18:27 +0800

    fix bug

--- src/pages/video-game-tree/index.vue
+++ src/pages/video-game-tree/index.vue
@@ -319,7 +319,7 @@ export default {
             });
         },
         onExportToClip() {
-            navigator.clipboard.writeText(newClipText)
+            navigator.clipboard.writeText(this.treeData[0])
             .then(() => {
                 this.$message.success('写入成功');
             })
